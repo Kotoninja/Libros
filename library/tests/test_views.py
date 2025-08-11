922894fa94961641c234947936c4ba691ca6c06b
artem <SS4T46@yandex.ru>
2025-08-11 12:42:47 +0300

    Added views tests

---
+++ library/tests/test_views.py
@@ -0,0 +1,50 @@
+from django.test import TestCase
+from library.models import Book
+from django.urls import reverse
+
+
+class BookViewsTestHomePage(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        for i in range(20):
+            book = Book.objects.create(
+                title=f"Test{i}",
+                description=f"Description{i}",
+                price=100 * i,
+            )
+            book.add_tags(f"Tags{i}, Test{i}, Book{i}")
+
+    def test_status_code(self):
+        response = self.client.get(reverse("library:home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_html(self):
+        response = self.client.get(reverse("library:home"))
+        self.assertTemplateUsed(response, "library/home.html")
+
+    def test_query_set(self):
+        response = self.client.get(reverse("library:home"))
+        self.assertQuerySetEqual(response.context["books"], Book.objects.all())
+
+
+class BookViewsTestCreateBookPage(TestCase):
+    def test_create_book_form(self):
+        response = self.client.get(reverse("library:create_book"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "library/create_book.html")
+
+        response = self.client.post(
+            reverse("library:create_book"),
+            {"title": "Test", "description": "Description", "price": 100},
+        )
+        book = Book.objects.get(pk=1)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse("library:book", args=(book.pk,)))
+
+        response = self.client.get(reverse("library:book", args=(book.pk,)))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "library/book.html")
+        self.assertEqual(response.context["book"], Book.objects.get(pk=1))
+        
+
