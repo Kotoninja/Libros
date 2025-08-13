81ec7d4cc8340bebd53b7a53b7f2db7dedc36cb4
artem <SS4T46@yandex.ru>
2025-08-13 19:17:35 +0300

    fix

--- library/views.py
+++ library/views.py
@@ -21,8 +21,28 @@ def home(request):
 
 
 def book(request, id):
+    def get_rating(rating) -> list:
+        rating_list: list = []
+        rating = rating
+        i = 5
+        while i != 0:
+            if rating > 0:
+                if rating >= 1:
+                    rating_list.append(1)
+                else:
+                    rating_list.append(0.5)
+            else:
+                rating_list.append(0)
+            rating -= 1
+            i -= 1
+        return rating_list
+
     book = get_object_or_404(Book, pk=id)
-    return render(request, "library/book.html", context={"book": book})
+    return render(
+        request,
+        "library/book.html",
+        context={"book": book, "rating": get_rating(book.rating)},
+    )
 
 
 def create_book(request):
@@ -40,15 +60,15 @@ def create_book(request):
 
 
 def search(request):
-    context: dict = {"filter": AdditionalSearchFilter()}
+    context: dict = {"filter_form": AdditionalSearchFilter()}
 
     books = Book.objects.all()
 
-    form = SearchForm(request.GET or None)
-    filter = AdditionalSearchFilter(request.GET or None)
+    search_form = SearchForm(request.GET or None)
+    filter_form = AdditionalSearchFilter(request.GET or None)
 
-    if form.is_valid() and form.cleaned_data["search"]:
-        search: str = form.cleaned_data["search"]
+    if search_form.is_valid() and search_form.cleaned_data["search"]:
+        search: str = search_form.cleaned_data["search"]
         context |= {"search": search}
 
         books = books.filter(
@@ -57,19 +77,24 @@ def search(request):
             | Q(tags__name__in=search.split())
         ).distinct()
 
-    if filter.is_valid():
+    if filter_form.is_valid():
         if "is_rating_upper" in request.GET:
             books = books.filter(rating__gte=4.7)
-            
+
     paginator = Paginator(books, 18)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
     context |= {
         "amount": books.count(),
-        "search_form": SearchForm(initial={"search":'test'}),
+        "search_form": SearchForm(
+            initial={"search": search_form.cleaned_data["search"]}
+        ),
+        "filter_form": AdditionalSearchFilter(
+            initial={"is_rating_upper": request.GET.get("is_rating_upper","")}
+        ),
         "page_obj": page_obj,
         "paginator": paginator,
     }
-    
+
     return render(request, "library/search.html", context=context)
