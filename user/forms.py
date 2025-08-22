85d819665470c2ebef6cba516be445c1d9ba90ae
artem <SS4T46@yandex.ru>
2025-08-22 18:00:10 +0300

    update registration

--- user/forms.py
+++ user/forms.py
@@ -14,7 +14,28 @@ class LoginForm(forms.Form):
     )
     remember_me = forms.BooleanField(
         widget=forms.CheckboxInput(
-            attrs={"class": "form-check-input", "placeholder": "username"}
+            attrs={"class": "form-check-input", "placeholder": "Remember me"}
         ),
         required=False,
+    )
+
+
+class RegistrationForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Username"}
+        )
+    )
+    email = forms.EmailField(
+        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Email"})
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "Password"}
+        )
+    )
+    repeat_password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "Repeat password"}
+        )
     )
--- user/views.py
+++ user/views.py
@@ -1,34 +1,70 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .forms import LoginForm
-from django.contrib.auth import authenticate, login,logout
+from .forms import LoginForm, RegistrationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
 
 def login_user(request):
     context = {}
     if request.method == "POST":
         form = LoginForm(request.POST)
         if form.is_valid():
-            user = authenticate(request, username = form.cleaned_data["username"],password = form.cleaned_data["password"])
+            user = authenticate(
+                request,
+                username=form.cleaned_data["username"],
+                password=form.cleaned_data["password"],
+            )
             if user is not None:
                 login(request, user)
                 return redirect("library:home")
             else:
-                context |= {"auth_errors":"Incorrect username or password. Please submit the form again."}
+                context |= {
+                    "auth_errors": "Incorrect username or password. Please submit the form again."
+                }
         else:
             context |= {"errors": form.errors}
     else:
         form = LoginForm()
 
-    context |= {"form":form}
+    context |= {"form": form}
 
-    return render(request,"user/login.html",context=context)
+    return render(request, "user/login.html", context=context)
+
 
 def registration(request):
-    return render(request,"user/registration.html")
+    context = {}
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            if form.cleaned_data["password"] != form.cleaned_data["repeat_password"]:
+                context |= {
+                    "auth_errors": "Passwords did not occur. Please enter the same passwords in both fields."
+                }
+            elif User.objects.filter(username=form.cleaned_data["username"]).exists():
+                context |= {"auth_errors": "This username already exist"}
+            elif User.objects.filter(email=form.cleaned_data["email"]).exists():
+                context |= {"auth_errors": "This username already exist"}
+            else:
+                User.objects.create_user(
+                    username=form.cleaned_data["username"],
+                    email=form.cleaned_data["email"],
+                    password=form.cleaned_data["password"],
+                )
+                return redirect("user:login")
+        else:
+            context |= {"errors": form.errors}
+    else:
+        form = RegistrationForm()
+    context |= {"form": form}
+    return render(request, "user/registration.html", context=context)
 
+@login_required
 def profile(request):
     # return HttpResponse(request.user.username)
-    return render(request,"user/profile.html")
+    return render(request, "user/profile.html")
+
 
 def logout_user(request):
     logout(request)
