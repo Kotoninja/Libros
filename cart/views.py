0f81a44323703b3c4a185ced761e2817b29922a1
artem sckvor <ss4t46@yandex.com>
2025-09-17 22:48:30 +0300

    add product to cart

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from library.models import Book
+from django.views.decorators.http import require_POST
+from .cart import Cart
 
-# Create your views here.
+
+@require_POST
+def cart_add(request, book_id):
+    cart = Cart(request)
+    product = get_object_or_404(Book, id=book_id)
+    cart.add(product=product, quantity=1, update_quantity=True)
